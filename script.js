b5374729ecfa1fa65aa8bae2f80513604d5c8f5d
mohd anas <mohd75253@gmail.com>
2026-02-02 22:48:51 +0530

    trying new things

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
-// Get name from URL
-const params = new URLSearchParams(window.location.search);
+// Get name from URL path
+// Example: /valsells/anas
+const path = window.location.pathname.split("/");
 
+// path = ["", "valsells", "anas"]
+let name = path[path.length - 1];
 
-// Set question text
+// If user opens just /valsells/
+if (!name || name === "valsells") {
+  name = "YOU";
+}
+
+// Capitalize first letter
+name = name.charAt(0).toUpperCase() + name.slice(1);
+
+// Set text
 document.getElementById("question").innerText =
-  ` will you be my Valentine?`;
+  `${name}, will you be my Valentine?`;
 
 // No button movement
 const noBtn = document.getElementById("noBtn");
-noBtn.addEventListener("mouseenter", () => {
+noBtn.addEventListener("mouseover", () => {
   const x = Math.random() * 200 - 100;
   const y = Math.random() * 80 - 40;
   noBtn.style.transform = `translate(${x}px, ${y}px)`;
 });
 
 // Yes click
-function yesClicked() {
-  document.getElementById("card").innerHTML = `
-    <div class="emoji">❤️🎉</div>
-    <h1>YAYYY !</h1>
-    <img src="celebration.gif" alt="Celebration">
-    <p>You just made my heart melt 😍</p>
+function yesClick() {
+  document.querySelector(".card").innerHTML = `
+    <h1>❤️ Yayyy ${name}! ❤️</h1>
+    <p>You just made my day 😍</p>
   `;
 }
+
