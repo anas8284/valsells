9f54aef4bb5fe639b73f6ad283369f099b41ad4c
mohd anas <mohd75253@gmail.com>
2026-02-02 23:00:43 +0530

    ok

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
-// Get name from URL
-const params = new URLSearchParams(window.location.search);
-
-
-// Set question text
-document.getElementById("question").innerText =
-  ` will you be my Valentine?`;
-
-// No button movement
 const noBtn = document.getElementById("noBtn");
+
 noBtn.addEventListener("mouseenter", () => {
   const x = Math.random() * 200 - 100;
   const y = Math.random() * 80 - 40;
+
   noBtn.style.transform = `translate(${x}px, ${y}px)`;
 });
 
-// Yes click
 function yesClicked() {
   document.getElementById("card").innerHTML = `
     <div class="emoji">❤️🎉</div>
-    <h1>YAYYY !</h1>
-    <img src="celebration.gif" alt="Celebration">
+    <h1>YAYYYY!</h1>
     <p>You just made my heart melt 😍</p>
   `;
 }
+
